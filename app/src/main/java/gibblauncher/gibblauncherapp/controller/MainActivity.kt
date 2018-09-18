8a648c65f51d1570d3b7c4635175ea763d7ff52f
Marcelo Augusto <mekmay@hotmail.com>
2018-09-18 20:14:59 -0300

    Initializing project.

---
+++ app/src/main/java/gibblauncher/gibblauncherapp/controller/MainActivity.kt
@@ -0,0 +1,13 @@
+package gibblauncher.gibblauncherapp.controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import gibblauncher.gibblauncherapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
