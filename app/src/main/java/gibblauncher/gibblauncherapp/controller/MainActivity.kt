29f0242b6ef0b5378a9161331667d7724321bbe8
Ronyell Henrique <ronyellhenrique@gmail.com>
2018-09-22 19:07:19 -0300

    #10 - Refactoring table tennis activity

--- app/src/main/java/gibblauncher/gibblauncherapp/controller/MainActivity.kt
+++ app/src/main/java/gibblauncher/gibblauncherapp/controller/MainActivity.kt
@@ -9,7 +9,6 @@ import android.widget.RelativeLayout
 import gibblauncher.gibblauncherapp.R
 import gibblauncher.gibblauncherapp.model.BounceLocation
 import java.util.*
-import android.annotation.SuppressLint
 import android.graphics.Path
 import android.os.Handler
 import android.support.v4.content.ContextCompat
@@ -27,12 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
         getScreenMetrics()
->>>>>>> e7f8645... Adding bounce information in ball
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -51,15 +45,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    fun showBounceLocations(hawkeyeLayout: RelativeLayout) {
+    private fun showBounceLocations(hawkeyeLayout: RelativeLayout) {
         val listOfBounceLocation: MutableList<BounceLocation> = createBounceLocations()
 
-
         for ((index, bounceLocation) in listOfBounceLocation.withIndex()) {
-            if(bounceLocation.axisX >= 0 && bounceLocation.axisY >= 0){
+            if (bounceLocation.axisX >= 0 && bounceLocation.axisY >= 0) {
                 val imageView = ImageView(this)
-
 
                 imageView.x = tableTennisLocation[0] + bounceLocation.axisX
                 imageView.y = screenHeight + 50.0f
@@ -70,70 +61,66 @@ class MainActivity : AppCompatActivity() {
                 imageView.setImageResource(R.drawable.circle_ball)
 
                 imageView.setOnClickListener {
-                    //Your code here
-
-                    val toolTip = TextView(this)
-                    toolTip.text = "Jogada $index"
-                    val textColor = ContextCompat.getColor(this, R.color.colorText)
-                    toolTip.setTextColor(textColor)
-
-                    val layoutParams = RelativeLayout.LayoutParams(250,60)
-                    val shapeDrawable = ContextCompat.getDrawable(this, R.drawable.background_text)
-
-                    toolTip.gravity = Gravity.CENTER
-                    toolTip.setPaddingRelative( 0,0,0,5)
-                    toolTip.background = shapeDrawable
-
-
-                    if(imageView.x + 250 > screenWidth){
-                        toolTip.x = screenWidth - 270.0f
-                    } else if(imageView.x < 100) {
-                        toolTip.x = 0.0f
-                    } else {
-                        toolTip.x = imageView.x - 100
-                    }
-
-                    toolTip.y = imageView.y - 70
-
-                    hawkeyeLayout.addView(toolTip, layoutParams)
-
-                    Handler().postDelayed({
-                        hawkeyeLayout.removeView(toolTip)
-                    }, 1000L)
+                    showTipBounceLocation(index, imageView, hawkeyeLayout)
                 }
-
 
                 hawkeyeLayout.addView(imageView, params)
 
-                val path = Path()
-                path.moveTo(imageView.x + 200, screenHeight + 50.0f)
-                val y = tableTennisLocation[1] + bounceLocation.axisY
-                var sideCurve = 0
-
-                if(index % 2 == 0){
-                    sideCurve = -1
-                } else {
-                    sideCurve = 1
-                }
-
-                path.cubicTo(imageView.x,screenHeight + 50.0f,
-                        imageView.x + (300 * sideCurve), y + (screenHeight - y)/2,
-                        imageView.x,y)
-
-                ObjectAnimator.ofFloat(imageView, View.X,View.Y, path).apply {
-                    duration = 1000
-                    startDelay = 1000L * index
-                    start()
-                }
+                generateAnimationInBounceLocation(imageView, bounceLocation, index)
             }
-
-
-
-
         }
     }
 
-    fun createBounceLocations(): MutableList<BounceLocation> {
+    private fun generateAnimationInBounceLocation(imageView: ImageView, bounceLocation: BounceLocation, index: Int) {
+        val path = Path()
+        path.moveTo(imageView.x + 200, screenHeight + 50.0f)
+        val y = tableTennisLocation[1] + bounceLocation.axisY
+
+
+        val sideCurve = if (index % 2 == 0) -1 else 1
+
+        path.cubicTo(imageView.x, screenHeight + 50.0f,
+                imageView.x + (300 * sideCurve), y + (screenHeight - y) / 2,
+                imageView.x, y)
+
+        ObjectAnimator.ofFloat(imageView, View.X, View.Y, path).apply {
+            duration = 1000
+            startDelay = 1000L * index
+            start()
+        }
+    }
+
+    private fun showTipBounceLocation(index: Int, imageView: ImageView, hawkeyeLayout: RelativeLayout) {
+        val toolTip = TextView(this)
+        toolTip.text = "Jogada $index"
+        val textColor = ContextCompat.getColor(this, R.color.colorText)
+        toolTip.setTextColor(textColor)
+
+        val layoutParams = RelativeLayout.LayoutParams(250, 60)
+        val shapeDrawable = ContextCompat.getDrawable(this, R.drawable.background_text)
+
+        toolTip.gravity = Gravity.CENTER
+        toolTip.setPaddingRelative(0, 0, 0, 5)
+        toolTip.background = shapeDrawable
+
+        toolTip.x = if (imageView.x + 250 > screenWidth)
+            screenWidth - 270.0f
+        else if (imageView.x < 100)
+            0.0f
+        else
+            imageView.x - 100
+
+        toolTip.y = imageView.y - 70
+
+        hawkeyeLayout.addView(toolTip, layoutParams)
+
+        Handler().postDelayed({
+            hawkeyeLayout.removeView(toolTip)
+        }, 1000L)
+    }
+
+
+    private fun createBounceLocations(): MutableList<BounceLocation> {
         val listOfBounceLocation: MutableList<BounceLocation> = mutableListOf()
 
         for (index in 0..9) {
@@ -145,7 +132,7 @@ class MainActivity : AppCompatActivity() {
         return listOfBounceLocation
     }
 
-    fun getScreenMetrics() {
+    private fun getScreenMetrics() {
         val displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
 
@@ -153,7 +140,6 @@ class MainActivity : AppCompatActivity() {
         screenHeight = displayMetrics.heightPixels
     }
 
-    fun rand(s: Int, e: Int) = Random().nextInt(e + 1 - s) + s
+    private fun rand(initial: Int, final: Int) = Random().nextInt(final + 1 - initial) + initial
 
->>>>>>> d13b19f... Adding bounce points in view
 }
