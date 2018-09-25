68680a79c268b0abd888f6ba13c64fbc7537960b
Marcelo Augusto <mekmay@hotmail.com>
2018-09-24 22:51:26 -0300

    #7 - Adding initial layout to playFragment.

--- app/src/main/java/gibblauncher/gibblauncherapp/controller/PlayFragment.kt
+++ app/src/main/java/gibblauncher/gibblauncherapp/controller/PlayFragment.kt
@@ -1,20 +1,59 @@
 package gibblauncher.gibblauncherapp.controller
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.Toast
 
 import gibblauncher.gibblauncherapp.R
+import kotlinx.android.synthetic.main.fragment_play.*
 
-class PlayFragment : Fragment() {
+class PlayFragment : Fragment(), SeekBar.OnSeekBarChangeListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_play, container, false)
+       return inflater.inflate(R.layout.fragment_play, container, false)
     }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        seekBarFragmentPlay!!.setOnSeekBarChangeListener(this)
+    }
+
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        when(progress){
+            0 -> {
+                imageViewFragmentPlayLeft.visibility = View.VISIBLE
+                imageViewFragmentPlayCenter.visibility = View.INVISIBLE
+                imageViewFragmentPlayRight.visibility = View.INVISIBLE
+            }
+            1 -> {
+                imageViewFragmentPlayLeft.visibility = View.INVISIBLE
+                imageViewFragmentPlayCenter.visibility = View.VISIBLE
+                imageViewFragmentPlayRight.visibility = View.INVISIBLE
+            }
+            2 -> {
+                imageViewFragmentPlayLeft.visibility = View.INVISIBLE
+                imageViewFragmentPlayCenter.visibility = View.INVISIBLE
+                imageViewFragmentPlayRight.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+    }
+
+    fun Context.toast(message: CharSequence) =
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
