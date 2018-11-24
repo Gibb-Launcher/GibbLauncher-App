5e3f3d445c9458a588aea828e0d896f80041e860
Ronyell Henrique <ronyellhenrique@gmail.com>
2018-11-24 19:31:12 -0200

    Removing unused charts

--- app/src/main/java/gibblauncher/gibblauncherapp/controller/StatisticFragment.kt
+++ app/src/main/java/gibblauncher/gibblauncherapp/controller/StatisticFragment.kt
@@ -66,44 +66,9 @@ class StatisticFragment : Fragment() {
         listBounceLocation = realm.where<BounceLocation>().findAll()
 
 
-        clickListener()
         createPieChart()
-        createBarChartPosition()
-        createBarChartPlay()
         createRadarChart()
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    fun clickListener(){
-            btnInitialDate.setOnClickListener{
-                // Get Current Date
-                val c = Calendar.getInstance()
-                initialYear = c.get(Calendar.YEAR)
-                initialMonth = c.get(Calendar.MONTH)
-                initialDay = c.get(Calendar.DAY_OF_MONTH)
-
-
-                val datePickerDialog = DatePickerDialog(context,
-                        DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                            initialDate.setText(dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year) }, initialYear!!, initialMonth!!, initialDay!!)
-                datePickerDialog.show()
-            }
-
-        btnFinalDate.setOnClickListener{
-            // Get Current Date
-            val c = Calendar.getInstance()
-            finalYear = c.get(Calendar.YEAR)
-            finalMonth = c.get(Calendar.MONTH)
-            finalDay = c.get(Calendar.DAY_OF_MONTH)
-
-
-            val datePickerDialog = DatePickerDialog(context,
-                    DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                        finalDate.setText(dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year) }, finalYear!!, finalMonth!!, finalDay!!)
-            datePickerDialog.show()
-        }
-
-
     }
 
 
@@ -159,115 +124,6 @@ class StatisticFragment : Fragment() {
         pieChart.setUsePercentValues(true)
     }
 
-    private fun createBarChartPosition() {
-        val barEntries: MutableList<BarEntry> = mutableListOf()
-        val barEntries1: MutableList<BarEntry> = mutableListOf()
-
-        // TODO get plays per position
-        barEntries.add(BarEntry(1f, 100f))
-        barEntries.add(BarEntry(2f, 50f))
-        barEntries.add(BarEntry(3f, 58f))
-
-        barEntries1.add(BarEntry(1f, 50f))
-        barEntries1.add(BarEntry(2f, 71f))
-        barEntries1.add(BarEntry(3f, 30f))
-
-        val barDataSet = BarDataSet(barEntries, "Dentro")
-        barDataSet.setColors(ContextCompat.getColor(this.context, R.color.colorGreen))
-        val barDataSet1 = BarDataSet(barEntries1, "Fora")
-        barDataSet1.setColors(ContextCompat.getColor(this.context, R.color.colorRed))
-
-        val listOfPositions: MutableList<String> = mutableListOf("Esquerda", "Centro", "Direita")
-
-
-        val data = BarData(barDataSet, barDataSet1)
-        barChart.data = data
-
-        val xAxis: XAxis = barChart.xAxis
-        xAxis.valueFormatter = (IndexAxisValueFormatter(listOfPositions))
-        xAxis.position = (XAxis.XAxisPosition.BOTTOM)
-        xAxis.granularity = 1f
-        xAxis.setCenterAxisLabels(true)
-        xAxis.isGranularityEnabled = (true)
-
-        val barSpace = 0.0f
-        val groupSpace = 0.5f
-
-        data.barWidth = (0.25f)
-
-        barChart.axisRight.setDrawGridLines(false)
-        barChart.axisLeft.setDrawGridLines(false)
-        barChart.xAxis.setDrawGridLines(false)
-
-        barChart.xAxis.axisMinimum = 0f
-        barChart.description.isEnabled = false
-        barChart.groupBars(0f, groupSpace, barSpace)
-    }
-
-    private fun createBarChartPlay() {
-        // Create bars
-        // TODO get plays
-        val yValues: MutableList<BarEntry> = mutableListOf()
-        yValues.add(BarEntry(0f, 1f))
-        yValues.add(BarEntry(1f, 10f))
-        yValues.add(BarEntry(2f, 11f))
-        yValues.add(BarEntry(3f, 20f))
-        yValues.add(BarEntry(4f, 15f))
-        yValues.add(BarEntry(5f, 18f))
-        yValues.add(BarEntry(6f, 77f))
-
-        // Create a data set
-        val dataSet = BarDataSet(yValues, "")
-        dataSet.setDrawValues(true)
-
-        // Create a data object from the dataSet
-        val data = BarData(dataSet)
-        // Format data as percentage
-        data.setValueFormatter(PercentFormatter())
-
-        // Make the chart use the acquired data
-        horizontalBarChart.data = data
-
-        // Create the labels for the bars
-        val xVals: MutableList<String> = mutableListOf()
-        xVals.add("Jogada 1")
-        xVals.add("Jogada 2")
-        xVals.add("Jogada 3")
-        xVals.add("Jogada 4")
-        xVals.add("Jogada 5")
-        xVals.add("Jogada 6")
-        xVals.add("Jogada 99")
-
-        // Display labels for bars
-        horizontalBarChart.xAxis.valueFormatter = IndexAxisValueFormatter(xVals)
-
-        // Set the maximum value that can be taken by the bars
-        horizontalBarChart.axisLeft.axisMaximum = 100f
-
-        // Bars are sliding in from left to right
-        horizontalBarChart.animateXY(1000, 1000)
-        // Display scores inside the bars
-        horizontalBarChart.setDrawValueAboveBar(false)
-
-        // Hide grid lines
-        horizontalBarChart.axisLeft.isEnabled = false
-        horizontalBarChart.axisRight.isEnabled = false
-        // Hide graph description
-        horizontalBarChart.description.isEnabled = false
-        // Hide graph legend
-        horizontalBarChart.legend.isEnabled = false
-
-        horizontalBarChart.axisRight.setDrawGridLines(false)
-        horizontalBarChart.axisLeft.setDrawGridLines(false)
-        horizontalBarChart.xAxis.setDrawGridLines(false)
-
-
-        // Design
-        dataSet.setColors(*ColorTemplate.VORDIPLOM_COLORS)
-        data.setValueTextSize(13f)
-
-        horizontalBarChart.invalidate()
-    }
 
     private fun createRadarChart() {
         val entries: MutableList<RadarEntry> = mutableListOf()
